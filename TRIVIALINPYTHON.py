3d15c9a0414a240588da4d24184f63370b736d55
pardo13 <56310582+pardo13@users.noreply.github.com>
2019-12-10 18:08:10 +0100

    Add files via upload

---
+++ TRIVIALINPYTHON.py
@@ -0,0 +1,301 @@
+
+#Programa principal
+
+#Añadimos las preguntas, respuestas y la respuesta verdadera
+# de las categorías en el array FÁCIL
+facil=  [["ENTRETENIMIENTO","¿De qué planeta son originarios los eworks del universo de La guerra de las galaxias ?","1)Alderaan","2)Tatooine","3)Endor ",3],
+        ["ENTRETENIMIENTO","¿Quién protagonizó la película Descubriendo nunca jamás?","1)Brad Pitt","2)Johnny Deep","3)Leonardo Dicaprio ",2],
+        ["ENTRETENIMIENTO","¿Alrededor de qué ciudad se encontraba la Ruta del bakalao?","1)Madrid","2)Valencia","3)Barcelona ",2],
+        ["ENTRETENIMIENTO","¿Cómo se llama el primer álbum en directo que publicaron Joan Manuel Serrat y Joaquín Sabina?","1)La orquesta de Titanic","2)Dos pájaros de un tiro","3)Doscontraatacan ",2],
+        ["ENTRETENIMIENTO","¿Quién dirigió la película Los lunes al sol?","1)Fernando León de Aranoa","2)Pedro Almodóvar","3)Alejandro Amenábar ",1],
+        ["ARTE","¿Qué es el Renacimiento?","1) Campaña militar","2)Movimiento cultural","3)Estilos de cultura",2],
+        ["ARTE","¿Quién formuló la teoría Heliocéntrica, en la que la Tierra gira alrededor del Sol y no al revés?","1)Copérnico","2)Ptolomeo","3)Francisco de Quevedo",1],
+        ["ARTE","La única de las 7 maravillas del mundo que se conserva es","1)Faro de Alejandría","2)La torre de babel","3)pirámide de Guiza",3],
+        ["ARTE","El verdadero nombre del cuadro La mona Lisa es","1)La gioconda","2)La mona lissa","3)Luís de góngora",1],
+        ["ARTE","¿Dónde se ubica la Cueva de Altamira?","1)Asturias","2)Galicia","3)Cantabria",3],
+        ["GEOGRAFÍA E HISTORIA","¿En qué continente se ubica España?","1)Europa","2)America","3)Africa",1],
+        ["GEOGRAFÍA E HISTORIA","¿En qué continente se ubica Arabia Saudí?","1)Africa","2)Asia","3)Europa",2],
+        ["GEOGRAFÍA E HISTORIA","Qué océano limita con España:","1)Pacifico","2)Atlantico","3)Mediterraneo",2],
+        ["GEOGRAFÍA E HISTORIA","¿Cuál es el mayor continente por extensión terrestre?","1)Antártida","2)América","3)Asia",3],
+        ["GEOGRAFÍA E HISTORIA","¿En qué año termina la edad media y comienza la edad moderna?","1)1945","2)1492","3)1775",2]]
+        
+#Añadimos las preguntas, respuestas y la respuestaverdadera
+# de las categorías en el array NORMAL
+normal= [["ENTRETENIMIENTO","¿Cuántos premios Grammy ha ganado Britney Spears?","1)Uno","2)Dos ","3)Ninguno",1],
+        ["ENTRETENIMIENTO","¿Qué personaje famoso lidera la banda llamada Nancys Rubias?","1)Nacho Canut","2)Alaska ","3)Mario Vaquerizo ",3],
+        ["ENTRETENIMIENTO","¿En qué país nació la Bauhaus?","1)Alemania","2)Holanda ","3)Estados Unidos ",1],
+        ["ENTRETENIMIENTO","¿En qué año se estrenó la película de Disney Pinocho? ","1)1940","2)1950 ","3)1964 ",1],
+        ["ENTRETENIMIENTO","¿Cómo se llamaba el personaje que interpretaba John Travolta en Grease?","1)Daniel sanchez","2)Danny Zuko","3)Danny Grease",2],
+        ["ARTE","¿Quién es el autor de la Celestina?","1)Cervantes","2)Fernando de Rojas","3)Góngora",1],
+        ["ARTE","¿A qué religión ofrece culto la catedral de Santiago de Compostela?","1)Catolicismo","2)Ortodoxa","3)Islam",1],
+        ["ARTE","¿Dónde se ubica la Cueva de Altamira??","1)Asturias","2)Galicia","3)Cantabria",3],
+        ["ARTE","¿Cuál de estas NO es una de las 7 maravillas actuales del mundo?","1)Muralla china","2)la alhambra","3)Estatua de Cristo redentor",2],
+        ["ARTE","El Guernica es un cuadro de…","1)Rubens","2)Pablo Picasso","3)Van gogh",2],
+        ["GEOGRAFÍA E HISTORIA","¿Qué nombre ha recibido el Imperio Romano de Oriente?","1)imperio persa","2)imperio otomano","3)imperio bizantino",3],
+        ["GEOGRAFÍA E HISTORIA","Por qué lugares cruza el río Tajo:","1)España","2)España y portugal","3) Portugal, España y Francia",2],
+        ["GEOGRAFÍA E HISTORIA","Cómo se llamaba la civilización que habitaba la península itálica antes que los romanos:","1)Etruscos","2)Griegos","3)Celtas",1],
+        ["GEOGRAFÍA E HISTORIA","¿Cuántos continentes hay en el planeta Tierra?","1)5","2)6","3)Ninguna de las anteriores es correcta",2],
+        ["GEOGRAFÍA E HISTORIA","Cuál era el lugar de origen del emperador  romano Trajano:","1)Roma","2)Norte africa","3)Hispania",3]]
+
+#Añadimos las preguntas, respuestas y la respuesta verdadera
+# de las categorías en el array DIFÍCIL
+dificil=    [["ENTRETENIMIENTO","¿Qué quería comerse Alf?","1)A Willie","2)Al gato","3)Las plantas",2],
+            ["ENTRETENIMIENTO","¿Cuántos años tenía Mozart al morir?","1)35","2)45","3)54",1],
+            ["ENTRETENIMIENTO","¿En qué saga de videojuegos aparece la entrega Ocarina of time","1)Pokemon","2)Legend of Zelda","3)Grand theft Auto",2],
+            ["ENTRETENIMIENTO","Arroz con leche me quiero casar con una señorita de...","1)San Nicolás","2)San Martin","3)San José",2],
+            ["ENTRETENIMIENTO","¿Cómo se llama el papá de Simba en la película Rey León?","1)Scar","2)Mufasa","3)Zazú",2],
+            ["ARTE","El sobrenombre del pintor Doménikos Theotokópoulos es…","1)El Greco","2)El Griego","3)Domi",1],
+            ["ARTE","Jónico, dórico y corintio son…","1)Cuadros","2)Estilos de construcción romanos","3)Estilos de construcción griegos",3],
+            ["ARTE","En qué museo está expuesto el cuadro de “La Mona Lisa”:","1)Museo del Prado","2)Museo del Louvre","3) Museo de Versalles",2],
+            ["ARTE","¿Cuál fue el título original de este libro tan famoso?","1)El ingenioso hidalgo Don Quijote de la Mancha","2)El ingenioso hidalgo Don Qvixote de la Mancha","3) El ingenioso hidalgo Don Qvijote de la Mancha",2],
+            ["ARTE","¿Quién compuso el Cantar de Mío Cid?","1)Rodrigo Díaz de Vivar","2)El Cid","3)Es anónimo",3],
+            ["GEOGRAFÍA E HISTORIA","¿Contra quién batalló El Cid?","1)Contra los cristianos","2)Contra los moros","3)Contra los moros y contra los cristianos",3],
+            ["GEOGRAFÍA E HISTORIA","El almirante Blas de Lezo defendió Cartagena de Indias de…","1)Los franceses","2)Los ingleses","3)Los holandeses",2],
+            ["GEOGRAFÍA E HISTORIA","¿Cuál era el sobrenombre de Juana de Arco?","1)La Doncella de Orleans","2)La Libertadora de Francia","3)La Libertadora de Galia",1],
+            ["GEOGRAFÍA E HISTORIA","¿Quién de estos fue el primer rey de la Casa de Borbón?","1)Carlos V","2)Felipe V","3)Fernando el Católico",2],
+            ["GEOGRAFÍA E HISTORIA","¿Cuál era el sobrenombre de Atila el Huno?","1)El conquistador","2)El bárbaro","3)El azote de Dios",3]]
+
+#inicializamos variables varias de configuración de cada jugador
+jugador1 = [[0,0,False,1],[0]]
+jugador2 = [[0,0,False,2],[0]]
+jugador3 = [[0,0,False,3],[0]]
+jugador4 = [[0,0,False,4],[0]]
+jugador5 = [[0,0,False,5],[0]]
+jugador6 = [[0,0,False,6],[0]]
+
+puntuacion =[]
+turno=0
+jugador_eleccion = [-1]
+
+import random
+#from time import sleep
+#import os
+
+#La función temporizador no la podemos integrar
+#porque la tendríamos que ejecutar a la misma vez
+#introducimos la pregunta. 
+#def temporizadorTrivial():
+#temporizador 10 segundos, 10 a 0
+        #time=True
+        #for i in range(0,10):
+                #print(10 -i)
+                #sleep(1)
+                #os.system ("cls")
+                #salida=(10-i)
+        #time=False
+        #return time
+
+def mostrarMenu():
+ #función mostrar menú   
+        print('''
+                .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------. 
+                | .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |
+                | |  _________   | || |  _______     | || |     _____    | || | ____   ____  | || |     _____    | || |      __      | || |   _____      | |
+                | | |  _   _  |  | || | |_   __ \    | || |    |_   _|   | || ||_  _| |_  _| | || |    |_   _|   | || |     /  \     | || |  |_   _|     | |
+                | | |_/ | | \_|  | || |   | |__) |   | || |      | |     | || |  \ \   / /   | || |      | |     | || |    / /\ \    | || |    | |       | |
+                | |     | |      | || |   |  __ /    | || |      | |     | || |   \ \ / /    | || |      | |     | || |   / ____ \   | || |    | |   _   | |
+                | |    _| |_     | || |  _| |  \ \_  | || |     _| |_    | || |    \ ' /     | || |     _| |_    | || | _/ /    \ \_ | || |   _| |__/ |  | |
+                | |   |_____|    | || | |____| |___| | || |    |_____|   | || |     \_/      | || |    |_____|   | || ||____|  |____|| || |  |________|  | |
+                | |              | || |              | || |              | || |              | || |              | || |              | || |              | |
+                | '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |
+                 '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------' 
+        ''')
+        
+        print()
+        print("                                                          ==================================================")
+        print("                                                          <<               M   E   N   Ú                  >>")
+        print("                                                          ==================================================")
+        print()
+        print("                                                          --------------------------------------------------")
+        print("                                                          <                  1: Jugar                      >")
+        print("                                                          <------------------------------------------------>")
+        print("                                                          <                  2: Salir                      >")
+        print("                                                          --------------------------------------------------")
+        print() 
+    
+        salir = True
+        while salir==True:
+                print(" Iniciando juego >>")
+                print(" ________________________________________________")
+                menu = int(input(" Dime que opción deseas: "))
+                if menu == 1:
+		#elección de jugadores
+                        jugadores_validos = True
+                        while jugadores_validos == True:
+                                jugadores=input("¿Cuántos jugadores van a participar?: ")
+                                if (jugadores=="2" or jugadores=="3" 
+                                or jugadores=="4" or jugadores=="5" or jugadores=="6"):
+                                        jugadores=int(jugadores)
+                                        jugadores_validos = False
+                                else:
+#si no es correcto lo que ha escrito vuelve a preguntar
+                                        jugadores=input("¿Cuántos jugadores van a participar?: ")
+                                        
+                        juego(turno,jugadores)
+                elif menu == 2:
+                        salir = False
+
+def dado():        
+        dado=(random.randrange(14))
+        return dado
+
+def resetearPuntuacion():
+#función resetear función, ponemos la puntuación a 0
+        jugador1[0][0]=0
+        jugador2[0][0]=0
+        jugador3[0][0]=0
+        jugador4[0][0]=0
+        jugador5[0][0]=0
+        jugador6[0][0]=0
+        
+
+def juego (turno,jugadores_totales):
+ #define a qué jugador le toca en cada turno      
+        while turno<jugadores_totales*5:               
+                salir=False
+
+                if jugador_eleccion[0]<jugadores_totales-1:
+                        jugador_eleccion[0]+=1
+                else:
+                        jugador_eleccion[0]=0
+                if jugador_eleccion[0]==0:
+                        jugador=jugador1                                
+                        turno+=1
+                        
+                elif jugador_eleccion[0]==1:
+                        jugador=jugador2
+                        turno+=1
+                                
+                elif jugador_eleccion[0]==2:
+                        jugador=jugador3
+                        turno+=1
+                                
+                elif jugador_eleccion[0]==3:
+                        jugador=jugador4
+                        turno+=1
+                                
+                elif jugador_eleccion[0]==4:
+                        jugador=jugador5
+                        turno+=1
+                                
+                elif jugador_eleccion[0]==5:
+                        jugador=jugador6
+                        
+                while salir==False:                        
+                        if jugador[0][2]==False:   
+#validamos la opcion de nivel correctamente                             
+                                entrada=input("Nivel de dificultad: (fácil)-->opción 1,(normal)-->opción 2 o\n (difícil)-->opción 3 elije y escribe una ")                              
+                                if entrada =="1" or entrada =="2" or entrada =="3":
+                                        entrada=int(entrada)
+                                        if entrada ==1:
+                                                jugador[0][2]=facil
+                                        elif entrada ==2:
+                                                jugador[0][2]=normal
+                                        else: 
+                                                jugador[0][2]=dificil
+                                        salir=True
+                        else:
+                                salir=True
+                
+                tope_preguntas = len(jugador[1])
+                
+                tirada_dado=dado()
+                
+                for i in range (tope_preguntas-1):
+                        if jugador[1][i] == tirada_dado:
+                                tirada_dado=dado()
+             
+                pregunta = jugador[0][2]
+                puntos=0
+                racha=0
+                print ("\n")
+                print ("*****jugador" ,jugador[0][3],"es tu turno*****\n")
+                print ("La pregunta es de: ",pregunta[tirada_dado][0])
+                print (pregunta[tirada_dado][1])
+                print ("\n")
+                print (pregunta[tirada_dado][2])
+                print (pregunta[tirada_dado][3])
+                print (pregunta[tirada_dado][4])
+                print ("\n")
+
+                rachas = jugador[1][0]                
+                respuesta = input("Respuesta ")                
+                respuesta_valida =False
+               
+                while respuesta_valida == False:
+		#validamos que la respuesta sea válida
+                        if respuesta =="1" or respuesta =="2" or respuesta =="3":
+                                respuesta=int(respuesta)
+                                respuesta_valida=True
+                        else:
+                                print("Respuesta imposible!!--> vuelve a probar")
+                                respuesta = input("Respuesta ")
+
+                #comprobamos si la respuesta es correcta
+                if respuesta == pregunta[tirada_dado][5]:
+
+                    if pregunta == facil:
+		#en caso de acertar la pregunta
+		#cuando la racha sea mayor a dos se le añadirá
+		#puntos extra según el nivel
+                        if rachas >2:
+                            puntos = 6
+                        else:
+                            puntos=3
+                            racha=1
+                    elif pregunta == normal:
+                        if rachas >2:
+                            puntos=12
+                        else:
+                            puntos=6
+                            racha=1
+                    else:
+                        if racha>2:
+                            puntos=18
+                        else:
+                            puntos=9
+                            racha=1
+                else:
+	        
+                    puntos=0
+                    racha=0
+                    print("Respuesta no valida")
+	    #actualizamos las variables de puntos y rachas
+                puntos_base=jugador[0][0]
+                puntos_totales=puntos_base + puntos
+                racha_base=jugador[1][0]
+                racha_total=racha + racha_base
+
+	    #actualizamos los puntos y racha del jugador
+                jugador[0][0] = puntos_totales
+                jugador[0][1] = racha_total
+                jugador[1].append(tirada_dado)
+                print(jugador[0][0])
+                                
+        if turno>=jugadores_totales*5:
+	#cuando se acaban los turnos creamos el array de puntuaciones
+                puntuacion=[]
+                puntuacion.append(jugador1[0][0])
+                puntuacion.append(jugador2[0][0])
+                puntuacion.append(jugador3[0][0])
+                puntuacion.append(jugador4[0][0])
+                puntuacion.append(jugador5[0][0])
+                puntuacion.append(jugador6[0][0])
+         
+
+        tope=len(puntuacion)
+        maximo=0
+        #cuando la puntuación sea mayor al máximo
+        #entonces el for terminará e imprimirá el ganador
+        for i in range (tope):               
+                if puntuacion[i]>maximo:
+                        maximo=puntuacion[i]
+                        tope_total=i
+                                                        
+        print ("Jugador",tope_total+1,"es el ganador con",maximo,"PUNTOS \n")
+        resetearPuntuacion()
+        mostrarMenu()
+                                                
+mostrarMenu()
+
+
